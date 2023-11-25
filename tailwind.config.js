caf7f1cbf3fa6c76c51309e10a807cf823bd132b
tanto <tantows001@gmail.com>
2023-11-25 19:28:46 +0700

    feat: Gelaran InTO UGM

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'main':'#28C797',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
